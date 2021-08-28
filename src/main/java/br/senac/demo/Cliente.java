dda806a42f71b2a633201d7dafeaa8c3ab11c733
colombolucas <colombo.lucasit@gmail.com>
2021-08-27 21:24:04 -0300

    Novas atualizações cliente

--- src/main/java/br/senac/demo/Cliente.java
+++ src/main/java/br/senac/demo/Cliente.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Target;
+import org.springframework.web.bind.annotation.Mapping;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -16,6 +19,7 @@ import javax.persistence.Entity;
 public class Cliente {
 
 
+    @Id
     private String nome;
     private String nomeSocial;
     private String CPF;
